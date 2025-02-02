08b3db3407977ef6d7aaf700ade75d6e1dd4668f
gtopsis <csd3004@csd.uoc.gr>
2025-02-02 16:57:10 +0200

    feat(theme btn): create Floating btn for app's theme toggle btn

---
+++ src/components/FloatingThemeToggleButton.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { Colors } from '@/src/constants';
+import ThemeToggleButton from './ThemeToggleButton';
+import { useMantineColorScheme } from '@mantine/core';
+
+interface FloatingButtonProps {
+  dark: boolean;
+  children: React.ReactNode;
+}
+
+const FloatingButton = styled.div<FloatingButtonProps>`
+  position: fixed;
+  bottom: 120px;
+  right: 20px;
+  z-index: 10;
+  border-radius: 50%;
+  padding: 10px;
+  background-color: ${props => (props.dark ? Colors.colorSecondary : Colors.grey)};
+
+  @media (min-width: 768px) {
+    display: none; /* Hide on larger screens */
+  }
+`;
+
+const FloatingThemeToggleButton = () => {
+  const { colorScheme } = useMantineColorScheme();
+  const isDarkTheme = colorScheme === 'dark';
+
+  return (
+    <FloatingButton dark={isDarkTheme}>
+      <ThemeToggleButton />
+    </FloatingButton>
+  );
+};
+
+export default FloatingThemeToggleButton;
